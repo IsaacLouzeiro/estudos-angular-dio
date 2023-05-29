182b5d065fddc5178793c431055362471ab81911
IsaacLouzeiro <isaacelias1110@gmail.com>
2023-05-29 10:59:49 -0300

    finalizando o conteudo de rotas

---
+++ rotas/src/app/pages/portifolio/card/card.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  constructor(
+    private parametrizador: ActivatedRoute,
+    private navegador: Router
+  ) {
+
+    //http://localhost:4200/portfolio/{1}
+    this.parametrizador.params.subscribe(
+      res => console.log(res)
+    )
+
+    //http://localhost:4200/portfolio/{1}
+    this.parametrizador.firstChild?.params.subscribe(
+      res => console.log(res)
+    )
+
+    //http://localhost:4200/portfolio/1{name=isaac&token=123}
+    this.parametrizador.queryParams.subscribe(
+      res => console.log(res)
+    )
+  }
+
+  ngOnInit(): void {
+    // setInterval(() => {
+    //   this.navegador.navigate(['/'])
+    // }, 5000)
+  }
+
+}
